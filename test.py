7eadb19ae38a02209a68fffc1c7f2c70a1696a0a
prl7un <134267526+prl7un@users.noreply.github.com>
2026-06-25 01:23:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 def WrongNameConvention(): # 1. CamelCase 함수명 (PEP8 위반)
     a = 1                  # 2. 사용하지 않는 변수
-    print("test1")          # 3. 들여쓰기(스페이스 4칸) 규칙 위반
+    print("test3")          # 3. 들여쓰기(스페이스 4칸) 규칙 위반
